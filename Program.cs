721bb0e23564c7b12bce0a8be0ffc29233a6fdf7
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-04-13 15:26:28 +0500

    Создать базовый класс Person и его производный класс Student

--- Program.cs
+++ Program.cs
@@ -9,4 +9,24 @@ namespace MyprojecsApp
             Console.ReadKey();
         }
     }
+    class Person
+    {
+        public string LastName;
+        public string FirstName;
+        public string MiddleName;
+        public int BirthYear;
+        public string Nation;
+        public string Gender;
+        public int Age;
+        public string PlaceOfBirth;                 //место рождения
+        public string PlaceOfResidence;             //место жителство
+    }
+    class Student : Person
+    {
+        public string University;
+        public string Faculty;
+        public string Specialty;
+        public int Login;              //int потому что все логин у нас примерно в такой формат: 2017010136
+        public int Password;            //Логин и Пароль для проверка результат!!
+    }
 }
