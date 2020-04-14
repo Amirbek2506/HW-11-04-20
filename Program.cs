3e6f86121ffd3abf8492024b009b76b09bcea139
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-04-14 18:46:21 +0500

    add Course and Group

--- Program.cs
+++ Program.cs
@@ -26,6 +26,8 @@ namespace MyprojecsApp
         public string University;
         public string Faculty;
         public string Specialty;
+        public int Course;
+        public string Group;
         public int Login;              //int потому что все логин у нас примерно в такой формат: 2017010136
         public int Password;            //Логин и Пароль для проверка результат!!
     }
